a79b44b64c33aa0bba5b2f3c637222c8de5e6b35
Ryan Chipman <ryan@ryanchipman.com>
2018-10-02 18:56:05 -0400

    Create a route that serves from state

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,28 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+use rocket::{Rocket,State};
+
+struct Greeting {
+    text: String,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> String {
+    String::from("Hello, world!")
+}
+
+#[get("/dynamic")]
+fn dynamic(greeting: State<Greeting>) -> String {
+    greeting.text.clone()
+}
+
+fn rocket() -> Rocket {
+    rocket::ignite()
+        .manage(Greeting { text: String::from("Hello, dynamic world!") })
+        .mount("/", routes![index,dynamic])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
 }
