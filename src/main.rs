be55ccedb65730aab6513b2c493309f088a2ba0b
Ryan Chipman <ryan@ryanchipman.com>
2018-10-02 23:23:23 -0400

    Get a sql query working

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,53 @@
 #![plugin(rocket_codegen)]
 
 extern crate rocket;
+#[macro_use] extern crate diesel;
 
-use std::sync::RwLock;
-use rocket::{Rocket,State};
+table! {
+    greetings (id) {
+        id -> Nullable<Integer>,
+        text -> Text,
+    }
+}
 
-struct Greeting {
-    text: RwLock<String>,
+use std::ops::Deref;
+
+use rocket::{Rocket,State,Request,Outcome};
+use rocket::http::Status;
+use rocket::request::{self,FromRequest};
+
+use diesel::prelude::*;
+use diesel::sqlite::SqliteConnection;
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+
+type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
+static DATABASE_URL: &'static str = "/Users/ryan/git/personal/snake-web/tmp/db.sqlite";
+
+fn init_pool() -> SqlitePool {
+    let manager = ConnectionManager::<SqliteConnection>::new(DATABASE_URL);
+    Pool::new(manager).expect("db pool")
+}
+
+pub struct DbConn(pub PooledConnection<ConnectionManager<SqliteConnection>>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let pool = request.guard::<State<SqlitePool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+impl Deref for DbConn {
+    type Target = SqliteConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 #[get("/")]
@@ -16,23 +57,20 @@ fn index() -> String {
 }
 
 #[get("/dynamic")]
-fn dynamic(greeting: State<Greeting>) -> String {
-    greeting.text.read().unwrap().clone()
+fn dynamic(conn: DbConn) -> String {
+    use greetings::dsl::*;
+    let res = greetings.select(text).load::<String>(&*conn).unwrap();
+    format!("{:?}", res)
 }
 
 #[post("/greeting", data = "<input>")]
-fn set_greeting(input: String, greeting: State<Greeting>) -> String {
-    let mut text = greeting.text.write().unwrap();
-    *text = input.clone();
-    String::from("success!")
+fn set_greeting(input: String) -> String {
+    String::from("TODO: failed")
 }
 
 fn rocket() -> Rocket {
-    let greeting = Greeting{
-        text: RwLock::new(String::from("Hello, dynamic world!"))
-    };
     rocket::ignite()
-        .manage(greeting)
+        .manage(init_pool())
         .mount("/", routes![index,dynamic,set_greeting])
 }
 
