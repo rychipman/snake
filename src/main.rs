286b65054e6736a0e9c3d2c910c82643b98e7833
Ryan Chipman <ryan@ryanchipman.com>
2018-10-03 08:57:23 -0400

    Factor db stuff out into separate file

--- src/main.rs
+++ src/main.rs
@@ -5,47 +5,12 @@ extern crate rocket;
 #[macro_use] extern crate diesel;
 
 mod schema;
+mod db;
 
-use std::ops::Deref;
-
-use rocket::{Rocket,State,Request,Outcome};
-use rocket::http::Status;
-use rocket::request::{self,FromRequest};
+use rocket::{Rocket};
 
 use diesel::prelude::*;
 use diesel::insert_into;
-use diesel::sqlite::SqliteConnection;
-use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
-
-type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
-static DATABASE_URL: &'static str = "/Users/ryan/git/personal/snake-web/tmp/db.sqlite";
-
-fn init_pool() -> SqlitePool {
-    let manager = ConnectionManager::<SqliteConnection>::new(DATABASE_URL);
-    Pool::new(manager).expect("db pool")
-}
-
-pub struct DbConn(pub PooledConnection<ConnectionManager<SqliteConnection>>);
-
-impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
-    type Error = ();
-
-    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
-        let pool = request.guard::<State<SqlitePool>>()?;
-        match pool.get() {
-            Ok(conn) => Outcome::Success(DbConn(conn)),
-            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
-        }
-    }
-}
-
-impl Deref for DbConn {
-    type Target = SqliteConnection;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
 
 #[get("/")]
 fn index() -> String {
@@ -53,18 +18,18 @@ fn index() -> String {
 }
 
 #[get("/dynamic")]
-fn dynamic(conn: DbConn) -> String {
+fn dynamic(conn: db::Conn) -> String {
     use schema::greetings::dsl::*;
-    let res = greetings.select(text).load::<String>(&*conn).unwrap();
+    let res = greetings.select(text).load::<String>(conn.handler()).unwrap();
     format!("{:?}", res)
 }
 
 #[post("/greeting", data = "<input>")]
-fn set_greeting(input: String, conn: DbConn) -> String {
+fn set_greeting(input: String, conn: db::Conn) -> String {
     use schema::greetings::dsl::*;
     let res = insert_into(greetings)
         .values(text.eq(input.clone()))
-        .execute(&*conn);
+        .execute(conn.handler());
     match res {
         Ok(_) => String::from("success!"),
         Err(_) => String::from("failed"),
@@ -73,7 +38,7 @@ fn set_greeting(input: String, conn: DbConn) -> String {
 
 fn rocket() -> Rocket {
     rocket::ignite()
-        .manage(init_pool())
+        .manage(db::init_pool())
         .mount("/", routes![index,dynamic,set_greeting])
 }
 
