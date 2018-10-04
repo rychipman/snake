ce0eeb2d10491642435872d1c3dda82004f4a05e
Ryan Chipman <ryan@ryanchipman.com>
2018-10-03 23:40:04 -0400

    Delete greetings table from schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,19 +1,7 @@
 table! {
-    greetings (id) {
-        id -> Nullable<Integer>,
-        text -> Text,
-    }
-}
-
-table! {
     scores (id) {
         id -> Integer,
         score -> Integer,
         email -> Nullable<Text>,
     }
 }
-
-allow_tables_to_appear_in_same_query!(
-    greetings,
-    scores,
-);
